6e1f137a7c60231394ae0b2daaca58f8a9d83a14
Mark Wang <armstrong.wang2000@gmail.com>
2024-08-24 15:45:46 -0600

    Add project files.

---
+++ CSharpBeginers/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+
+    int a = 99;
+    string b = "abc";
+    byte c = 255;
+    char d = 'a';
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
